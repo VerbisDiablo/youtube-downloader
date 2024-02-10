3e3a3a0e07f690ad9784999fbc63d04290450768
Verbis Diablo <90982301+VerbisDiablo@users.noreply.github.com>
2024-02-10 20:19:24 +0100

    Add files via upload

--- download.py
+++ download.py
@@ -4,13 +4,13 @@ from pytube import YouTube
 from moviepy.editor import AudioFileClip
 import tkinter as tk
 from tkinter import font
+from tkinter import filedialog
 
 # ----------FUNCTIONS----------
 
 # Function to clear text
 def clearText(text):
     text.delete('1.0','end')
-
 
 # Function to download youtube video
 def downloadVideo(link,label):
@@ -19,11 +19,12 @@ def downloadVideo(link,label):
     filename = "".join([c for c in youtubeObject.title if c.isalpha() or c.isdigit() or c==' ']).rstrip() + ".mp4"
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
-        youtubeObject.download()
+        youtubeObject.download(output_path=directory)
         label.config(text="Download finished: " + filename)
     except:
         label.config(text="An error has occurred")
 
+# Function to download MP3 from youtube video
 def downloadSong(link, label):
 
     inp= link.get("1.0","end-1c")
@@ -32,14 +33,14 @@ def downloadSong(link, label):
     yt = YouTube(inp)
     stream = yt.streams.filter(only_audio=True).first()
     temp_filename = "temp.mp4"
-    stream.download(filename=temp_filename)
+    stream.download(output_path=directory, filename=temp_filename)
 
     # Use the video title as the filename for the MP3 file
     filename = "".join([c for c in yt.title if c.isalpha() or c.isdigit() or c==' ']).rstrip() + ".mp3"
 
     # Convert the video to MP3
     clip = AudioFileClip(temp_filename)
-    clip.write_audiofile(filename)
+    clip.write_audiofile(f"{directory}/{filename}")
 
     # Clean up the temporary file
     clip.close()
@@ -48,19 +49,30 @@ def downloadSong(link, label):
     # Update the label
     label.config(text="Download finished: " + filename)
 
-#test
+# Function that calls downloadSong or downloadVideo
 def download(link,label):
     if(var.get() == "MP4"):
         downloadVideo(link,label)
     else:
         downloadSong(link,label)
-# ----------GUI----------
 
+# Function to choose directory
+def choose_directory():
+    global directory
+    directory = filedialog.askdirectory()
+    print(directory)
+
+# ----------GUI----------
+# Store directory
 
 
 # Create the window
 window = tk.Tk()
 window.title("Youtube Video Downloader")
+
+# Create frame for input text and clear button
+frame = tk.Frame(window)
+frame.pack()
 
 # Font
 Font = font.Font(family="Helvetica", size=14)
@@ -69,14 +81,12 @@ Font = font.Font(family="Helvetica", size=14)
 var = tk.StringVar(value="MP3")
 
 # Options
-mp3_button = tk.Radiobutton(window, text="MP3", variable=var, value="MP3")
-mp4_button = tk.Radiobutton(window, text="MP4", variable=var, value="MP4")
-mp3_button.pack()
-mp4_button.pack()
+mp3_button = tk.Radiobutton(frame, text="MP3", variable=var, value="MP3")
+mp4_button = tk.Radiobutton(frame, text="MP4", variable=var, value="MP4")
+mp3_button.pack(side=tk.LEFT)
+mp4_button.pack(side=tk.LEFT)
 
-# Create frame for input text and clear button
-frame = tk.Frame(window)
-frame.pack()
+
 
 
 # Create a label for download status
@@ -86,7 +96,7 @@ status.pack()
 # Create the text input case
 linktovideo = tk.Text(frame,
                     height=2,
-                    width=50)
+                    width=20)
 linktovideo.pack(side=tk.LEFT)
 
 # Create clear text button
@@ -98,7 +108,16 @@ clear = tk.Button(frame,
                     command=lambda: clearText(linktovideo))
 clear.pack(side=tk.RIGHT)
 
-#Create the button
+# Create the choose directory button
+chooseDirectory = tk.Button(frame,
+                            text="Choose Directory",
+                            font= Font,
+                            padx=10,
+                            pady=10,
+                            command=choose_directory)
+chooseDirectory.pack(side=tk.RIGHT)
+
+# Create the download button
 button = tk.Button(window,
                     text="Download",
                     font=Font,
